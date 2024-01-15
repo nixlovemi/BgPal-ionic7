b5caf31d5cd6a6bec539396aeeaf0b272f1c518a
Leandro Parra <nixlovemi@gmail.com>
2024-01-15 16:12:07 -0500

    chore: init

---
+++ capacitor.config.ts
@@ -0,0 +1,12 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'BgPal',
+  webDir: 'www',
+  server: {
+    androidScheme: 'https'
+  }
+};
+
+export default config;
